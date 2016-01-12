171d9d92d33ef6a7e36ed2ba2215918dd0bd8f1e
zhangshaolong <zhangshaolong@didichuxing.com>
2016-01-12 11:03:27 +0800

    支持本地多级路由查找

--- local-server/router.js
+++ local-server/router.js
@@ -9,22 +9,33 @@ var through = require('through2');
 var URL = require('url');
 var fs = require('fs');
 var path = require('path');
-var htmlProcessor = require('./html');
+var htmlMerge = require('./html').htmlMerge;
 var routerConfig = require('./routerConfig');
 
-module.exports = function (req, res, next) {
-
-    var pathname = URL.parse(req.url).pathname;
-    var redictPath = routerConfig[pathname];
-    if (redictPath) {
-        var encoding = 'UTF-8';
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        var tplContent = fs.readFileSync(path.resolve('view/' + redictPath));
-        var content = new String(tplContent, encoding);
-        res.end(htmlProcessor.htmlMerge(content, encoding));
-    } else if (pathname.indexOf('.html') > 0) {
-        htmlProcessor.htmlProxy(req, res, next);
+/**
+ * 支持多级定向查找，直到找到.html文件或者未找到为止
+ */
+var lookupRedirectHtml = function (urlPath, res) {
+    var redirectPath = routerConfig[urlPath];
+    if (redirectPath) {
+        if (redirectPath.indexOf('.html') > 0) {
+            var encoding = 'UTF-8';
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            var tplContent = fs.readFileSync(path.resolve('view/' + redirectPath));
+            var content = new String(tplContent, encoding);
+            res.end(htmlMerge(content, encoding));
+        } else {
+            return lookupRedirectHtml(redirectPath, res);
+        }
     } else {
+        return false;
+    }
+}
+
+module.exports = function (req, res, next) {
+    var pathname = URL.parse(req.url).pathname;
+    var matched = lookupRedirectHtml(pathname, res);
+    if (matched === false) {
         return next();
     }
 };
