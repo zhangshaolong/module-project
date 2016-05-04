fe27b4978cb3a09e6bc816c497b0fb1b432aa4db
zhangshaolong <zhangshaolong@didichuxing.com>
2016-05-04 17:19:41 +0800

    a

--- mock/web_pagelist.js
+++ mock/web_pagelist.js
@@ -4,28 +4,28 @@ module.exports = function (params) {
         data: {
             list: [
                 {
-                    page_url: 'http://omega.xiaojukeji.com/crash/list?app_id=10000',
+                    page_url: 'http://omega.jizi.com/crash/list?app_id=10000',
                     page_name: 'crash列表页',
                     pvs: 100000,
                     uvs: 300,
                     ips: 500
                 },
                 {
-                    page_url: 'http://omega.xiaojukeji.com/crash/detail?app_id=10000&msgid=3237923d-88fe-46ec-90f9-91efac5c6533',
+                    page_url: 'http://omega.jizi.com/crash/detail?app_id=10000&msgid=3237923d-88fe-46ec-90f9-91efac5c6533',
                     page_name: 'crash详情页',
                     pvs: 100000,
                     uvs: 300,
                     ips: 500
                 },
                 {
-                    page_url: 'http://omega.xiaojukeji.com/crash/list?app_id=10000',
+                    page_url: 'http://omega.jizi.com/crash/list?app_id=10000',
                     page_name: 'crash列表页',
                     pvs: 100000,
                     uvs: 300,
                     ips: 500
                 },
                 {
-                    page_url: 'http://omega.xiaojukeji.com/crash/list?app_id=10000',
+                    page_url: 'http://omega.jizi.com/crash/list?app_id=10000',
                     page_name: 'crash列表页',
                     pvs: 100000,
                     uvs: 300,
